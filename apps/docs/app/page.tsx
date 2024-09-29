27cf2e147db6d4948ac8d9a127cd7d7226489039
Satnammeena2004 <satnam.meena2004@gmail.com>
2024-09-29 18:57:38 +0530

    rest of commit

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -6,14 +6,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+      <h1>DOCS</h1>
         <ol>
           <li>
             Get started by editing <code>app/page.tsx</code>
